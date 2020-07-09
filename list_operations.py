dfd33d779853c69ba92f3996bc26ad7cf28642bb
Anastasia Correa <correa.anastasia7@gmail.com>
2020-07-09 19:49:18 +0000

    Head, tail, last, top, first_three, last_five, middle, inner_four, inner_four_end, replace_head, replace_third_andlast, replace_middle, delete_middle

--- list_operations.py
+++ list_operations.py
@@ -188,6 +188,9 @@ def delete_third_and_seventh(input_list):
 
     """
 
+    del input_list[2]
+    del input_list[6]
+
     pass
 
 
@@ -204,6 +207,8 @@ def delete_middle(input_list):
     True
 
     """
+
+    del input_list[2:-2]
 
     pass
 
